6be033a9aea76c9bff0e39dface547f8d478c593
Galen Hancock <galenhancock@Galens-MacBook-Pro.local>
2018-12-11 22:55:10 -0500

    Made additions to init

--- Global_Economic_Comparisons/__init__.py
+++ Global_Economic_Comparisons/__init__.py
@@ -1,1 +1,7 @@
+#The Global Economic Comparisons package pre-processes, analyzes, visualizes income data
+
+__author__ = "Manoah, Nando, Chi, Galen"
+__version__ = '0.0'
+__status__ = "prototype"
+
 
